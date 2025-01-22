dbc00c48f48d4e653a57739b93c13e39e0022441
asavaliya6 <aditisavaliya60@gmail.com>
2025-01-22 19:41:09 +0530

    first commit

---
+++ app/Http/Middleware/IsAdminMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Log;
+
+class IsAdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (! auth()->user()->is_admin) { 
+            Log::error('Access denied: user is not an admin.', ['user_id' => auth()->id()]);
+            abort(403);
+        }
+        
+        return $next($request);
+    }
+}
